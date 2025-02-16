12062900030a562b5e57719d998f136ad28aaf75
SHIMIZUATSUKI <atklikmngkr.33@gmail.com>
2025-02-16 20:22:23 +0900

    first commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/shimizuatsuki/Desktop/Tech0/Advanced3/GitHub/Advanced3_app_test_frontend/src/app/layout\":[\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
